0826c87dc528e0294b3e40b5f6154000537bcce4
hoyeon <hoyun293@naver.com>
2021-06-27 12:38:50 +0900

    job #7 ADD: globalStyle에 Font 추가

--- src/utils/globalStyle.jsx
+++ src/utils/globalStyle.jsx
@@ -1,5 +1,33 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-    
+  heading1: {
+    fontFamily: 'NotoSansKRBold',
+    fontSize: 22,
+    lineHeight: 34,
+  },
+  heading2: {
+    fontFamily: 'NotoSansKRBold',
+    fontSize: 16,
+  },
+  header: {
+    fontFamily: 'NotoSansKRMedium',
+    fontSize: 20,
+  },
+  body1: {
+    fontFamily: 'NotoSansKRMedium',
+    fontSize: 16,
+  },
+  body2: {
+    fontFamily: 'NotoSansKRMedium',
+    fontSize: 14,
+  },
+  body2Bold: {
+    fontFamily: 'NotoSansKRBold',
+    fontSize: 14,
+  },
+  button: {
+    fontFamily: 'NotoSansKRBold',
+    fontSize: 18,
+  },
 })
