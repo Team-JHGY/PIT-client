ff1ebdb01b1fabdcb9a26fdc380e9fdefb2fa248
hoyeon <hoyun293@naver.com>
2021-07-04 21:59:34 +0900

    job #8 ADD: 트레이너 회원가입 페이지 - STEP1 렌더링 개선

---
+++ src/components/SignUp/MemberCard.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import { WithLocalSvg } from 'react-native-svg'
+import Member from '../../../assets/img/SignUp/member.svg'
+import CheckButton from '../../../assets/img/SignUp/checkButton.svg'
+import CheckButtonSelected from '../../../assets/img/SignUp/checkButtonSelected.svg'
+
+import globalStyle from '../../utils/globalStyle'
+const TrainerCard = ({ isChecked }) => {
+  return (
+    <View
+      style={
+        isChecked === true
+          ? [styles.cardSelected, styles.card]
+          : [styles.cardUnSelected, styles.card]
+      }
+    >
+      <View style={styles.checkWrapper}>
+        {isChecked === true ? (
+          <WithLocalSvg asset={CheckButtonSelected} width={30} height={30}></WithLocalSvg>
+        ) : (
+          <WithLocalSvg asset={CheckButton} width={30} height={30}></WithLocalSvg>
+        )}
+      </View>
+      <View style={styles.imageWrapper}>
+        <WithLocalSvg asset={Member} height={180} width={300}></WithLocalSvg>
+      </View>
+      <Text style={globalStyle.heading2}>회원</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderStyle: 'solid',
+    borderRadius: 10,
+    marginLeft: 5,
+    marginRight: 20,
+    alignItems: 'center',
+    flex: 1,
+  },
+  cardSelected: {
+    borderWidth: 2,
+    borderColor: '#2AFF91',
+  },
+  cardUnSelected: {
+    borderWidth: 1,
+    borderColor: '#E1EAF9',
+  },
+  checkWrapper: {
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    marginLeft: 14,
+    marginTop: 14,
+  },
+  heading2: {
+    ...globalStyle.heading2,
+  },
+})
+export default TrainerCard
---
+++ src/components/SignUp/TrainerCard.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import { WithLocalSvg } from 'react-native-svg'
+import Trainer from '../../../assets/img/SignUp/trainer.svg'
+import CheckButton from '../../../assets/img/SignUp/checkButton.svg'
+import CheckButtonSelected from '../../../assets/img/SignUp/checkButtonSelected.svg'
+import globalStyle from '../../utils/globalStyle'
+const TrainerCard = ({ isChecked }) => {
+  return (
+    <View
+      style={
+        isChecked === true
+          ? [styles.cardSelected, styles.card]
+          : [styles.cardUnSelected, styles.card]
+      }
+    >
+      <View style={styles.checkWrapper}>
+        {isChecked === true ? (
+          <WithLocalSvg asset={CheckButtonSelected} width={30} height={30}></WithLocalSvg>
+        ) : (
+          <WithLocalSvg asset={CheckButton} width={30} height={30}></WithLocalSvg>
+        )}
+      </View>
+      <View style={styles.imageWrapper}>
+        <WithLocalSvg asset={Trainer} height={180} width={300}></WithLocalSvg>
+      </View>
+      <Text style={globalStyle.heading2}>트레이너</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderStyle: 'solid',
+    borderRadius: 10,
+    marginLeft: 20,
+    marginRight: 5,
+    alignItems: 'center',
+    flex: 1,
+  },
+  cardSelected: {
+    borderWidth: 2,
+    borderColor: '#2AFF91',
+  },
+  cardUnSelected: {
+    borderWidth: 1,
+    borderColor: '#E1EAF9',
+  },
+  checkWrapper: {
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    marginLeft: 14,
+    marginTop: 14,
+  },
+  heading2: {
+    ...globalStyle.heading2,
+  },
+})
+export default TrainerCard
