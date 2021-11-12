106531c4d75a750c82082d891eb0fd0b0acea1cd
hoyeon <hoyun293@naver.com>
2021-11-13 08:22:10 +0900

    job #33 날짜별 스케쥴 조회 api 연결 (트레이너 스케쥴 함수 초안작성 )

--- src/components/Schedule/ViewBody.jsx
+++ src/components/Schedule/ViewBody.jsx
@@ -4,22 +4,26 @@ import { WithLocalSvg } from 'react-native-svg'
 
 // utils
 import globalStyle from '../../utils/globalStyle'
-import { getDayOfWeek, getMonthOfDate, getDayOfDate, getTimeOfDate } from '../../utils/commonFunctions'
+import {
+  getDayOfWeek,
+  getMonthOfDate,
+  getDayOfDate,
+  getTimeOfDate,
+} from '../../utils/commonFunctions'
 
 // JWT token
 import { UserContext } from '../../store/user'
-import config from "../../utils/config" 
-import { decode } from 'js-base64';
-
+import config from '../../utils/config'
+import { decode } from 'js-base64'
 
 // assets
 import addFloating from '../../../assets/img/Schedule/addFloating.svg'
 
 const ViewBody = ({ navigation, selectedDate }) => {
   const [lessonsInfo, setLessonsInfo] = useState([])
-  const { userState, userDispatch }   = React.useContext(UserContext)
-  const splitJwt                      = userState.jwtToken.split(".")
-  const userInfo                      = React.useState(JSON.parse(decode(splitJwt[1])))
+  const { userState, userDispatch } = React.useContext(UserContext)
+  const splitJwt = userState.jwtToken.split('.')
+  const userInfo = React.useState(JSON.parse(decode(splitJwt[1])))
 
   let strToday =
     getMonthOfDate(selectedDate) +
@@ -28,9 +32,9 @@ const ViewBody = ({ navigation, selectedDate }) => {
     ' (' +
     getDayOfWeek(selectedDate) +
     ')'
-  React.useEffect(()=>{
+  React.useEffect(() => {
     GetMonthTrainerSchedule(userState.jwtToken)
-  },[])
+  }, [])
 
   useEffect(() => {
     // dummy
@@ -51,42 +55,75 @@ const ViewBody = ({ navigation, selectedDate }) => {
 
   //해당 날짜 데이터 가져오기
   async function GetMonthTrainerSchedule(token) {
-
-    await fetch(`${config.BASE_URL}/schedules/trainer/${userInfo[0].sub}?day=${new Date(selectedDate).getDate()}&month=${Number(new Date(selectedDate).getMonth())+1}&year=${new Date(selectedDate).getFullYear()}`,{
-      method: 'GET', // *GET, POST, PUT, DELETE, etc.
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'include', // include, *same-origin, omit
-      headers: {
-          'Authorization' : token,
-          'Content-Type'  : 'application/json',
-          
+    await fetch(
+      `${config.BASE_URL}/schedules/trainer/${userInfo[0].sub}?day=${new Date(
+        selectedDate
+      ).getDate()}&month=${Number(new Date(selectedDate).getMonth()) + 1}&year=${new Date(
+        selectedDate
+      ).getFullYear()}`,
+      {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'include', // include, *same-origin, omit
+        headers: {
+          Authorization: token,
+          'Content-Type': 'application/json',
+        },
       }
-    })
-    .then((res) => res.json())
-    .then((res) => {
-        
-        if(res.code ===  0){
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.code === 0) {
           setLessonsInfo(res.data)
-          console.log("스케줄 : ", res)
-          console.log("스케줄 : ", res.data)
-        }else{
+          console.log('스케줄 : ', res)
+          console.log('스케줄 : ', res.data)
+        } else {
           console.log(res)
         }
-        
-        
-    })
-    .catch((e) => console.log(e))  
+      })
+      .catch((e) => console.log(e))
+  }
+
+  // 트레이너의 스케쥴이 몇일날에 있는지 조회하기
+  async function GetTrainerScheduleDates(token) {
+    await fetch(
+      `${config.BASE_URL}/schedules/day/${userInfo[0].sub}?day=${new Date(
+        selectedDate
+      ).getDate()}&month=${Number(new Date(selectedDate).getMonth()) + 1}&year=${new Date(
+        selectedDate
+      ).getFullYear()}`,
+      {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'include', // include, *same-origin, omit
+        headers: {
+          Authorization: token,
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.code === 0) {
+          setLessonsInfo(res.data)
+          console.log('스케줄 : ', res)
+          console.log('스케줄 : ', res.data)
+        } else {
+          console.log(res)
+        }
+      })
+      .catch((e) => console.log(e))
   }
 
   React.useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
       GetMonthTrainerSchedule(userState.jwtToken)
-    });
-    return unsubscribe;
-  }, [navigation]);
+    })
+    return unsubscribe
+  }, [navigation])
 
   return (
-    <View style={{ marginBottom: 20, alignSelf: 'stretch', flex: 1 ,  flexGrow: 1}}>
+    <View style={{ marginBottom: 20, alignSelf: 'stretch', flex: 1, flexGrow: 1 }}>
       <Text style={styles.date}>{strToday}</Text>
       {lessonsInfo.length === 0 ? (
         <Text style={styles.placeholder}>등록된 스케쥴이 없습니다.</Text>
@@ -100,38 +137,41 @@ const ViewBody = ({ navigation, selectedDate }) => {
                 <Pressable
                   onPress={() => {
                     if (item.numOfLesson !== null) {
-                      navigation.navigate('ScheduleDetailInfo', { type: 'reserved', id:item.id })
+                      navigation.navigate('ScheduleDetailInfo', { type: 'reserved', id: item.id })
                     } else {
-                      navigation.navigate('ScheduleDetailInfo', { type: 'notAvailable', id:item.id })
+                      navigation.navigate('ScheduleDetailInfo', {
+                        type: 'notAvailable',
+                        id: item.id,
+                      })
                     }
                   }}
                 >
                   {item.numOfLesson !== null ? (
                     <View style={[globalStyle.row, styles.scheduleInfo]}>
                       <View>
-                        {item.partnership.member.user.profileImage === null?
-                            <Image
+                        {item.partnership.member.user.profileImage === null ? (
+                          <Image
                             style={[styles.userImg]}
-                              source={require('../../../assets/img/SignUp/emptyProfile.png')}
-                            />
-                        :
-                            <Image source={{uri:`${item.partnership.member.user.profileImage.path}`}} style={[styles.userImg]}/>
-                        }   
+                            source={require('../../../assets/img/SignUp/emptyProfile.png')}
+                          />
+                        ) : (
+                          <Image
+                            source={{ uri: `${item.partnership.member.user.profileImage.path}` }}
+                            style={[styles.userImg]}
+                          />
+                        )}
                       </View>
                       <View style={[globalStyle.col_2]}>
                         <Text
                           style={[globalStyle.body2, globalStyle.textDartGery, styles.textmargin]}
                         >
-                          {getTimeOfDate(new Date(item.startAt))}
-                          ~
+                          {getTimeOfDate(new Date(item.startAt))}~
                           {getTimeOfDate(new Date(item.endAt))}
-                          
                         </Text>
 
                         <Text
                           style={[globalStyle.body2, styles.textmargin]}
                         >{`${item.partnership.member.user.name} (${item.sequence}번째 수업)`}</Text>
-                        
                       </View>
                     </View>
                   ) : (
@@ -142,18 +182,29 @@ const ViewBody = ({ navigation, selectedDate }) => {
                         <Text
                           style={[globalStyle.body2, globalStyle.textDartGery, styles.textmargin]}
                         >
-                           {new Date(item.startAt).getHours >12?
-                            "오전"+new Date(item.startAt).getHours()+":"+new Date(item.startAt).getMinutes()
-                            :
-                            "오후"+new Date(item.startAt).setHours(new Date(item.startAt).getHours()-12).getHours()+":"+new Date(item.startAt).getMinutes()
-                           }
-                           ~
-                           {
-                            new Date(item.endAt).getHours >12?
-                            "오전"+new Date(item.endAt).getHours()+":"+new Date(item.endAt).getMinutes()
-                            :
-                            "오후"+new Date(item.endAt).setHours(new Date(item.endAt).getHours()-12).getHours()+":"+new Date(item.endAt).getMinutes()
-                          }
+                          {new Date(item.startAt).getHours > 12
+                            ? '오전' +
+                              new Date(item.startAt).getHours() +
+                              ':' +
+                              new Date(item.startAt).getMinutes()
+                            : '오후' +
+                              new Date(item.startAt)
+                                .setHours(new Date(item.startAt).getHours() - 12)
+                                .getHours() +
+                              ':' +
+                              new Date(item.startAt).getMinutes()}
+                          ~
+                          {new Date(item.endAt).getHours > 12
+                            ? '오전' +
+                              new Date(item.endAt).getHours() +
+                              ':' +
+                              new Date(item.endAt).getMinutes()
+                            : '오후' +
+                              new Date(item.endAt)
+                                .setHours(new Date(item.endAt).getHours() - 12)
+                                .getHours() +
+                              ':' +
+                              new Date(item.endAt).getMinutes()}
                         </Text>
 
                         <Text style={[globalStyle.body2, styles.textmargin]}>{`${item.name}`}</Text>
@@ -198,8 +249,7 @@ const styles = StyleSheet.create({
     marginBottom: 0,
     marginLeft: 'auto',
     marginRight: 30,
-    alignSelf:'flex-end',
-
+    alignSelf: 'flex-end',
   },
   userImg: {
     width: 44,
